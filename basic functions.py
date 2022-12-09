777d6db600c5c2204f822405cd4a8e6421c26301
scyline <107317997+scyline@users.noreply.github.com>
2022-12-09 19:57:00 +0100

    Update basic functions.py

--- basic functions.py
+++ basic functions.py
@@ -14,3 +14,7 @@ def cost(M,W1,W2,b1,b2,V):
       for j in range(V):
         J = J + weight(M[i,j])*(np.dot(np.transpose(W1)[i,],np.transpose(np.transpose(W2)[j,])) + b1[i,0] + b2[j,0] - np.log(M[i,j]+1))**2
     return J[0,0]  
+
+  
+  
+  
