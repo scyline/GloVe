bb37cb318defd7cc4402cd6be829311dde46147e
scyline <107317997+scyline@users.noreply.github.com>
2023-01-19 22:05:15 +0100

    Update gradients.py

--- gradients.py
+++ gradients.py
@@ -1,16 +1,15 @@
 #compute the coefficient matrix
-def coefficient(M, W1, W2, B1, B2, V): 
+def coefficient(M, W1, W2, B1, B2, V): #OK
   fM = np.vectorize(weight)(M)
   logM = np.log(M+1) #add 1 to avoid log(0)
   ones = np.matrix([1 for x in range(V)]) #row of one
-  return 2*np.multiply(fM,
-                       np.dot(np.transpose(W1),W2)+np.dot(B1,ones)+np.dot(np.transpose(ones),np.transpose(B2))-logM)
+  return 2*np.multiply(fM, np.dot(W1,np.transpose(W2))+np.dot(B1,ones)+np.dot(np.transpose(ones),np.transpose(B2))-logM)
 
 #compute gradient of the biais
-def grad_B(coeff, V): 
-  ones = np.matrix([1 for x in range(V)]) 
+def grad_B(coeff, V): #OK
+  ones = np.matrix([1 for x in range(V)]) #row of one
   return np.dot(coeff, np.transpose(ones))
 
 #compute gradient of the weights
-def grad_W(coeff, W2, V): 
-  return np.dot(W2, np.transpose(coeff))
+def grad_W(coeff, W2, V): #OK
+  return np.dot(coeff, W2)
